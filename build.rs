d1a1718f3aecf28207f52bf17c693536ad864f25
Vladimir Vukicevic <vladimir@pobox.com>
2015-09-22 12:16:40 -0400

    Use system version of fontconfig via pkg-config if it's of an appropriate version

--- build.rs
+++ build.rs
@@ -2,11 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate pkg_config;
+
 use std::process::Command;
 use std::env;
 
-
 fn main() {
+    // if the system version of fontconfig is at least 2.11.1, use it
+    if pkg_config::Config::new().atleast_version("2.11.1").find("fontconfig").is_ok() {
+        return;
+    }
+
     assert!(Command::new("make")
         .args(&["-R", "-f", "makefile.cargo"])
         .status()
